5e9b4ea935b2ff55252db6332ac482317c31c0ea
sam <samuelmovi@protonmail.com>
2019-08-31 20:38:54 +0200

    jquery photo slide now working

--- my_site/static/my_site/js/test.js
+++ my_site/static/my_site/js/test.js
@@ -1,38 +1,31 @@
 $(document).ready(()=>{
+    // responsive top bar
 	var contactBurger =  $("#contact-burger");
 	var contactBar = $("#contact-bar");
 	contactBurger.click(function(){
 		contactBar.toggleClass("responsive");
 	});
+	// responsive navbar
 	var navbarBurger =  $("#navbar-burger");
 	var navBar = $("#navbar");
 	navbarBurger.click(function(){
 		navBar.toggleClass("responsive");
 	});
-
+    // photo carousel
     var allPhotos = $("#carousel").find('.carousel-photo');
-    var i;
-    for (i = 0; i < allPhotos.length; i++) {
-        allPhotos[i].hide();
-        var source = allPhotos[i].children().css();
-        alert(source);
-    }
-
-    /*
-     var photoIndex = 0;
-    function carousel() {
-      var i;
-      for (i = 0; i < allPhotos.length; i++) {
-        allPhotos[i].hide();
-      }
-      photoIndex++;
-      if (photoIndex > allPhotos.length) {
+    var photoIndex = 0;
+    function slidePhotos(){
+        var i;
+        for (i = 0; i < allPhotos.length; i++) {
+        allPhotos.eq(i).hide();
+        }
+        photoIndex++;
+        if (photoIndex > allPhotos.length) {
         photoIndex = 0;
-      }
-      allPhotos[photoIndex-1].show();
-      setTimeout(carousel, 2000); // Change image every 2.5 seconds
+        }
+        allPhotos.eq(photoIndex-1).show();
+        setTimeout(slidePhotos, 2000); // Change image every 2.5 seconds
     }
+    slidePhotos();
 
-    carousel();
-    */
 });
