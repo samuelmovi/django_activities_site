7a6996e4aaea0d14e61132dd2f07d7b62073cbae
sam <samuelmovi@protonmail.com>
2019-08-31 16:17:39 +0200

    bar burgers working

--- my_site/static/my_site/js/test.js
+++ my_site/static/my_site/js/test.js
@@ -1,20 +1,12 @@
 $(document).ready(()=>{
-
-    // contact bar
 	var contactBurger =  $("#contact-burger");
 	var contactBar = $("#contact-bar");
 	contactBurger.click(function(){
 		contactBar.toggleClass("responsive");
 	});
-
-    // carousel
-
-    // navbar
 	var navbarBurger =  $("#navbar-burger");
 	var navBar = $("#navbar");
 	navbarBurger.click(function(){
 		navBar.toggleClass("responsive");
 	});
-
-
 });
