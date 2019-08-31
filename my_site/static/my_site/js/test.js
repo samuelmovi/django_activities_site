b743c67f3d933cfda10370ad051648d92e9ca7d7
sam <samuelmovi@protonmail.com>
2019-08-31 20:18:07 +0200

    not managing to redo the slide show in jquery, yet

--- my_site/static/my_site/js/test.js
+++ my_site/static/my_site/js/test.js
@@ -9,4 +9,30 @@ $(document).ready(()=>{
 	navbarBurger.click(function(){
 		navBar.toggleClass("responsive");
 	});
+
+    var allPhotos = $("#carousel").find('.carousel-photo');
+    var i;
+    for (i = 0; i < allPhotos.length; i++) {
+        allPhotos[i].hide();
+        var source = allPhotos[i].children().css();
+        alert(source);
+    }
+
+    /*
+     var photoIndex = 0;
+    function carousel() {
+      var i;
+      for (i = 0; i < allPhotos.length; i++) {
+        allPhotos[i].hide();
+      }
+      photoIndex++;
+      if (photoIndex > allPhotos.length) {
+        photoIndex = 0;
+      }
+      allPhotos[photoIndex-1].show();
+      setTimeout(carousel, 2000); // Change image every 2.5 seconds
+    }
+
+    carousel();
+    */
 });
